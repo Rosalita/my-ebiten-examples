34142ff71194cecf9fda79f15e99f27145d4a48f
Rosie Hamilton <rosie.hamilton@thingco.com>
2019-03-14 16:24:12 +0000

    move menulist to own package

--- my-packages/menulist/menulist.go
+++ my-packages/menulist/menulist.go
@@ -1,42 +1,31 @@
-package main
+package menulist
 
-import (
+import(
+	"log"
 	"errors"
 	"image/color"
-	"log"
-	"os"
-
-	"github.com/golang/freetype/truetype"
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil" // required for debug text
-	"github.com/hajimehoshi/ebiten/examples/resources/fonts"
-	"github.com/hajimehoshi/ebiten/inpututil" // required for isKeyJustPressed
 	"github.com/hajimehoshi/ebiten/text"
 	"golang.org/x/image/font"
+	"github.com/golang/freetype/truetype"
+	"github.com/hajimehoshi/ebiten/examples/resources/fonts"
 )
 
-type gameState int
+var mplusNormalFont font.Face
 
-const (
-	titleScreen gameState = iota
-	options
-	play
-	quit
-)
+func init() {
+	tt, err := truetype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// define some kind of palette?
-var (
-	green1  = &color.NRGBA{0x00, 0x38, 0x40, 0xff}
-	green2  = &color.NRGBA{0x00, 0x5a, 0x5b, 0xff}
-	green3  = &color.NRGBA{0x00, 0x73, 0x69, 0xff}
-	green4  = &color.NRGBA{0x00, 0x8c, 0x72, 0xff}
-	green5  = &color.NRGBA{0x02, 0xa6, 0x76, 0xff}
-	purple1 = &color.NRGBA{0x30, 0x28, 0x40, 0xff}
-	purple2 = &color.NRGBA{0x47, 0x39, 0x5b, 0xff}
-	purple3 = &color.NRGBA{0x5f, 0x49, 0x73, 0xff}
-	purple4 = &color.NRGBA{0x7b, 0x58, 0x8c, 0xff}
-	purple5 = &color.NRGBA{0x99, 0x69, 0xa6, 0xff}
-)
+	const dpi = 72
+	mplusNormalFont = truetype.NewFace(tt, &truetype.Options{
+		Size:    24,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+}
 
 // MenuItem represents an item in a menu list
 type MenuItem struct {
@@ -51,8 +40,6 @@ type MenuItem struct {
 	SelTxtColour *color.NRGBA  // optional selected text colour, overrides default selected text colour
 }
 
-// TO DO - Text features
-// Support default text alignment: left, right, centre
 
 // MenuList is a navigatable, selectable menu
 type MenuList struct {
@@ -218,146 +205,5 @@ func (m *MenuList) Draw(screen *ebiten.Image) {
 
 		screen.DrawImage(item.image, opts)
 		opts.GeoM.Translate(m.Offx, m.Offy)
-	}
-}
-
-var (
-	state           gameState
-	playImage       *ebiten.Image
-	optionsImage    *ebiten.Image
-	quitImage       *ebiten.Image
-	square          *ebiten.Image
-	mplusNormalFont font.Face
-	mplusBigFont    font.Face
-	mainMenu        MenuList
-)
-
-func init() {
-	tt, err := truetype.Parse(fonts.MPlus1pRegular_ttf)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	const dpi = 72
-	mplusNormalFont = truetype.NewFace(tt, &truetype.Options{
-		Size:    24,
-		DPI:     dpi,
-		Hinting: font.HintingFull,
-	})
-}
-
-func update(screen *ebiten.Image) error {
-
-	screen.Fill(color.NRGBA{0x00, 0x00, 0x00, 0xff})
-
-	if state == titleScreen {
-
-		ebitenutil.DebugPrint(screen, "Title screen")
-		mainMenu.Draw(screen)
-
-		if inpututil.IsKeyJustPressed(ebiten.KeyUp) {
-			mainMenu.DecrementSelected()
-		}
-		if inpututil.IsKeyJustPressed(ebiten.KeyDown) {
-			mainMenu.IncrementSelected()
-		}
-
-		if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
-			switch mainMenu.GetSelectedItem() {
-			case "playButton":
-				state = play
-			case "optionButton":
-				state = options
-			case "quitButton":
-				os.Exit(0)
-			}
-			return nil
-		}
-
-	}
-
-	if state == play {
-		ebitenutil.DebugPrint(screen, "Play screen")
-
-		if square == nil {
-			square, _ = ebiten.NewImage(32, 32, ebiten.FilterNearest)
-		}
-		someColor := &color.NRGBA{0x7f, 0xff, 0x00, 0xff}
-		square.Fill(someColor)
-
-		opts := &ebiten.DrawImageOptions{}
-		opts.GeoM.Translate(64.0, 64.0)
-		screen.DrawImage(square, opts)
-
-		if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
-			state = titleScreen
-			return nil
-		}
-
-	}
-
-	if state == options {
-		ebitenutil.DebugPrint(screen, "Options screen")
-
-		if square == nil {
-			square, _ = ebiten.NewImage(32, 32, ebiten.FilterNearest)
-		}
-		someColor := &color.NRGBA{0x8a, 0x2b, 0xe2, 0xff}
-		square.Fill(someColor)
-
-		opts := &ebiten.DrawImageOptions{}
-		opts.GeoM.Translate(64.0, 64.0)
-		screen.DrawImage(square, opts)
-
-		if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
-			state = titleScreen
-			return nil
-		}
-	}
-
-	return nil
-}
-
-func main() {
-
-	newMenuItems := []MenuItem{
-		{Name: "playButton",
-			Text:     "PLAY",
-			TxtX: 36,
-			TxtY: 25,
-			BgColour: green1},
-		{Name: "optionButton",
-			Text:     "OPTIONS",
-			TxtX: 12,
-			TxtY: 25,
-			BgColour: green2},
-		{Name: "quitButton",
-			Text:     "QUIT",
-			TxtX: 36,
-			TxtY: 25,
-			BgColour: green3},
-	}
-
-	newMenuInput := MenuListInput{
-		Width:              128,
-		Height:             36,
-		Tx:                 128,
-		Ty:                 128,
-		DefaultSelBGColour: purple3,
-		MenuItems:          newMenuItems,
-	}
-
-	newMenu, err := NewMenu(newMenuInput)
-
-	if err != nil {
-		log.Printf("unable to create menu: %+v\n", err)
-	}
-
-	mainMenu = newMenu
-
-	state = titleScreen
-
-	if err := ebiten.Run(update, 400, 300, 2, "State!"); err != nil {
-		panic(err)
 	}
 }
